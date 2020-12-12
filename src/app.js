56b356d146c42e2fdca12408c76701f581727b39
Jonno Katahanas <jonathan.katahanas@outlook.com>
2020-12-12 12:09:53 +1000

    Updated js to make callback work.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 console.log("app is running");
 
-
+// Use name to find and extract symbol
 const onSearchSubmit = (event) => {
   event.preventDefault();
   console.log('event', event);
@@ -12,30 +12,49 @@ const onSearchSubmit = (event) => {
   // Get stock symbol
   const pendingStockSymbol = $.ajax(
     `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${stockName}&apikey=EU18KIHPYJ49OM7D`
-  ).then(stockSearchData => {
+  ).then((stockSearchData) => {
     console.log("stockSearchData", stockSearchData);
     const stockSymbol = stockSearchData.bestMatches[0][`1. symbol`];
     console.log("stockSymbol", stockSymbol);
-
+    findStockData(stockSymbol);
   });
-  
-    const pendingStockData = $.ajax(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${pendingStockSymbol}&apikey=EU18KIHPYJ49OM7D`
-        ).then(stockData => {
-          console.log("stockData", stockData);
-          const timeSeriesValues = Object.values(stockData['Time Series (Daily)']);
-          console.log(timeSeriesValues);
-          const lastCloseIndex = timeSeriesValues.length - 1;
-          console.log(timeSeriesValues[lastCloseIndex]['4. close']);
-          const lastClosePrice = timeSeriesValues[lastCloseIndex]['4. close'];
-          $("#result").append(lastClosePrice);
-        });
 
-  };
-
-
+}; 
+const findStockData = (symbol) => {
+  const pendingStockData = $.ajax(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&apikey=EU18KIHPYJ49OM7D`
+  ).then(stockData => {
+    console.log("stockData", stockData);
+    const timeSeriesValues = Object.values(stockData['Time Series (Daily)']);
+    console.log(timeSeriesValues);
+    const lastClose = timeSeriesValues[0];
+    console.log(lastClose);
+    $("#result").empty();
+    $("#result").append(lastClose['4. close']);
+  });
 
 };
+
 $("#stock-form").on("submit", onSearchSubmit)
+
+
+  
+//     const pendingStockData = $.ajax(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${pendingStockSymbol}&apikey=EU18KIHPYJ49OM7D`
+//         ).then(stockData => {
+//           console.log("stockData", stockData);
+//           const timeSeriesValues = Object.values(stockData['Time Series (Daily)']);
+//           console.log(timeSeriesValues);
+//           const lastCloseIndex = timeSeriesValues.length - 1;
+//           console.log(timeSeriesValues[lastCloseIndex]['4. close']);
+//           const lastClosePrice = timeSeriesValues[lastCloseIndex]['4. close'];
+//           $("#result").append(lastClosePrice);
+//         });
+
+//   };
+
+
+
+// };
+// $("#stock-form").on("submit", onSearchSubmit)
 
 
 
@@ -110,4 +129,4 @@ $("#stock-form").on("submit", onSearchSubmit)
        
     // };
     // $("#stock-form").on("submit", onSearchSubmit)
-    
+  
